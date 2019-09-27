df3a2800bf0392652b0adf8e76d9fd3b7391d52a
adgai19 <gaitondeaditya@gmail.com>
2019-09-28 01:26:50 +0530

    post request done.only get request is remaining/

--- routes/dastruct.js
+++ routes/dastruct.js
@@ -1,12 +1,13 @@
 const express = require('express');
 let router = express.Router();
+const dbStruct=require('../model/db');
 //let plainTextParser = require('plaintextparser');
 router.get("/", (req, res) => {
         res.send('hello world')
 }
 )
 // module.exports=router;
-router.route('/lat')
+router.route('/entry')
 .post(function(req,res){
     var chunk = '';
 
@@ -16,53 +17,40 @@ router.route('/lat')
 
     req.on('end', function(){
 
-        console.log(chunk); //just show in console
-    })
-    res.send(null);
+        console.log(chunk);
+        test=JSON.parse(chunk);
+        console.log(test.imagepath);
+         //just show in console
+         const newentry = new dbStruct({
+                 location:test.location,
+                 locality:test.locality,
+                 imagepath:test.imagepath,
+                 
+         });
+         newentry.save().then(data=>{
+                 res.json(data);
+         }).catch(err=>{
+                 rest.json({message:error})
+         })
 
-});
-router.route('/long')
-.post(function(req,res){
-    var chunk = '';
+         console.log(newentry);
+    });
 
-    req.on('data', function(data){
-        chunk += data; // here you get your raw data.
-    })        
-
-    req.on('end', function(){
-
-        console.log(chunk); //just show in console
-    })
-    res.send(null);
-
-});router.route('/imagepath')
-.post(function(req,res){
-    var chunk = '';
-
-    req.on('data', function(data){
-        chunk += data; // here you get your raw data.
-    })        
-
-    req.on('end', function(){
-
-        console.log(chunk); //just show in console
-    })
-    res.send(null);
-
-});router.route('/path')
-.post(function(req,res){
-    var chunk = '';
-
-    req.on('data', function(data){
-        chunk += data; // here you get your raw data.
-    })        
-
-    req.on('end', function(){
-
-        console.log(chunk); //just show in console
-    })
-    res.send(null);
+    res.send("entry recieved");
 
 });
 
+router.get('/getpost', (req,rec)=>{
+        console.log("sending posts");
+        
+                dbStruct.find((err,posts)=>{
+                        if(err) return console.error(err);
+                        console.log(posts);
+                })
+                        
+               // posts=JSON.parse(posts);
+               
+                console.log("posts sent");
+        
+})
 module.exports = router;
