5ee981845ca1e093871eb2ae2d1b8b999cb222a2
Pritesh Alias Vaibhav Naik <41779472+Pro4tech@users.noreply.github.com>
2019-09-27 22:13:08 +0530

    Try this

--- server.js
+++ server.js
@@ -4,11 +4,18 @@ const bodyParser=require('body-parser');
 let app=express();
 let dbroutes=require('./routes/dastruct.js');
 app.use(dbroutes);
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+/* Don't use express as its not supported any longer*/
+//app.use(express.urlencoded({ extended: true }));
+//app.use(express.json());
 // app.use(express.json())
 // app.use(bodyParser.urlencoded({ extended: false }))
 // app.use(bodyParser.text({ type:text/plain }));
+// --------------new edits--------------------------
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+//---------------------------------------------------
 mongos.connect('localhost:27017', { useNewUrlParser: true },()=>console.log("mongoose connected"));
 //app.use(express.static('public'));
 app.listen(3001,()=>console.log("server running"));
